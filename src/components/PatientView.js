f37036ab3a78dfb3abd22c1154d260976e0b9f76
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-03-19 12:50:32 +0530

    uis

--- src/components/PatientView.js
+++ src/components/PatientView.js
@@ -84,22 +84,7 @@ const PatientView = () => {
       </Stack>
     );
   return <></>;
-        <div class="flex justify-between p-8 mx-6 my-4 shadow-slate-200 shadow-2xl rounded-2xl">
-      <div class="flex">
-        <div class="mr-8">
-          <img src={Report} alt="Doc Image" class="pt-6" />
-        </div>
-        <div flex>
-          <p class="text-2xl font-bold p-1">{title}</p>
-          <p class="text-xl p-1">{date}</p>
-        </div>
-      </div>
-      <div className="py-10">
-        <Button variant="contained">View</Button>
-      </div>
-    </div>
-    </Stack>
-  )
+  
 };
 
 export default PatientView;
